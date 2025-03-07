194d42755607a2d34a8e2774ba1a75473c9c9e0c
mwHyung <mw.hyung@eluocnc.com>
2025-03-07 10:55:57 +0900

    commit

--- src/components/pages/app/HomePage/index.tsx
+++ src/components/pages/app/HomePage/index.tsx
@@ -418,7 +418,7 @@ const HomePage = () => {
               id="user-list-table"
               columns={contentsColumns}
               rows={selectedUser}
-              selectedRows={selectedUser ? [selectedUser.id] : []}
+              // selectedRows={selectedUser ? [selectedUser.id] : []}
               containerClassName="main_table"
               onRowSelect={handleSelectRow}
               colgroup={["15%", "60%", "10%", "15%"]}
